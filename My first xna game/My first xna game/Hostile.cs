a691da891150aeb363d579bfb0e5e5f6653dc82e
chrome99 <firefox1112@hotmail.com>
2015-06-06 11:18:26 +0300

    okay, this is truly the last commit

--- My first xna game/My first xna game/Hostile.cs
+++ My first xna game/My first xna game/Hostile.cs
@@ -72,7 +72,23 @@ namespace My_first_xna_game
             {
                 //set new health
                 int oldHealth = stats.health;
-                int newHealth = stats.health - source.stats.strength + stats.defence;
+                int newHealth;
+                Player player = this as Player;
+                if (player != null)
+                {
+                    if (player.defendingTimer.Counting)
+                    {
+                        newHealth = oldHealth;
+                    }
+                    else
+                    {
+                        newHealth = stats.health - source.stats.strength + stats.defence;
+                    }
+                }
+                else
+                {
+                    newHealth = stats.health - source.stats.strength + stats.defence;
+                }
                 if (newHealth > -1)
                 {
                     stats.health = newHealth;
@@ -96,15 +112,13 @@ namespace My_first_xna_game
                 }
 
                 //update player hud and menu
-                Player player = this as Player;
                 if (player != null)
                 {
                     player.HandleHit(oldHealth - newHealth);
                 }
 
                 //timer
-                cooldownTimer.timerSwitch = true;
-                cooldownTimer.counter = 0f;
+                cooldownTimer.Reset();
             }
         }
 
